90f82764cad0805bbc50e778398d3945cea2ff54
Danilo Carlo <danilocsgomes98@gmail.com>
2022-12-16 14:48:22 -0300

    Update

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use PHPUnit\TextUI\XmlConfiguration\Group;
-use app\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
