a07900840748268db096aabbb3d66cfc3659a434
Danilo Carlo <danilocsgomes98@gmail.com>
2022-12-26 14:57:54 -0300

    update

--- database/migrations/2022_12_26_173639_create_products_table.php
+++ database/migrations/2022_12_26_173639_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->double('price', 10, 2);
+            $table->text('description');
+            $table->enum('status', ['Ativo', 'Inativo'])->default('Ativo');
             $table->timestamps();
         });
     }
